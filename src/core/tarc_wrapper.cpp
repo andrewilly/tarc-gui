8cd560e0f60011e28cd997d33c005a9a017b7a2f
andrewilly <italian.hitech@gmail.com>
2026-04-08 12:58:27 +0200

    Update tarc_wrapper.cpp

--- src/core/tarc_wrapper.cpp
+++ src/core/tarc_wrapper.cpp
@@ -1,7 +1,8 @@
 #include "tarc_wrapper.h"
+#include <QThread>
 #include <QDebug>
 
-// Questi includono sono per il core TARC
+// Questi include sono per il core TARC
 #include "engine.h"
 #include "io.h"
 #include "license.h"
@@ -19,11 +20,8 @@ bool TarcWrapper::compress(const QString& archive, const QStringList& files, boo
     Q_UNUSED(level)
     Q_UNUSED(error)
     
-    // TODO: Implementare chiamata reale al core TARC
-    // Per ora simuliamo progresso
     for (int i = 0; i <= 100; i += 10) {
         emit progressUpdated(i, QString("Compressing... %1%").arg(i));
-        // Simula lavoro
         QThread::msleep(100);
     }
     
@@ -36,7 +34,6 @@ bool TarcWrapper::extract(const QString& archive, const QString& targetDir, QStr
     Q_UNUSED(targetDir)
     Q_UNUSED(error)
     
-    // TODO: Implementare chiamata reale al core TARC
     for (int i = 0; i <= 100; i += 10) {
         emit progressUpdated(i, QString("Extracting... %1%").arg(i));
         QThread::msleep(100);
@@ -51,7 +48,6 @@ bool TarcWrapper::remove(const QString& archive, const QStringList& files, QStri
     Q_UNUSED(files)
     Q_UNUSED(error)
     
-    // TODO: Implementare chiamata reale al core TARC
     emit progressUpdated(100, "Removing files...");
     
     return true;
@@ -62,7 +58,6 @@ bool TarcWrapper::test(const QString& archive, QString& error)
     Q_UNUSED(archive)
     Q_UNUSED(error)
     
-    // TODO: Implementare chiamata reale al core TARC
     for (int i = 0; i <= 100; i += 20) {
         emit progressUpdated(i, QString("Testing... %1%").arg(i));
         QThread::msleep(100);
